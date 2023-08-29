5a20ce4e97fc9039df576e7b7806b04d79d737dd
Jyoti <jyotisingh82000@gmail.com>
2023-08-29 17:17:46 +0530

    creating model and working on admin panel and connecting the product

--- mysite/shop/models.py
+++ mysite/shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    product_id = models.AutoField
+    product_name = models.CharField(max_length=30)
+    product_des = models.CharField(max_length=80)
+    product_date = models.DateField()

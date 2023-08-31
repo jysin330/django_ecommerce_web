f8b4ce7c4fba47ccb8b93d60307038462e436e13
Jyoti <jyotisingh82000@gmail.com>
2023-08-31 16:16:04 +0530

    learn about the template block element and extends

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -5,3 +5,27 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'shop/index1.html')
+
+
+def about(request):
+    return render(request, 'shop/about1.html')
+
+
+def contact(request):
+    return render(request, 'shop/contact1.html')
+
+
+def tracker(request):
+    return render(request, 'shop/tracker1.html')
+
+
+def search(request):
+    return render(request, 'shop/search1.html')
+
+
+def productview(request):
+    return render(request, 'shop/productview.html')
+
+
+def checkout(request):
+    return render(request, 'shop/checkout1.html')
